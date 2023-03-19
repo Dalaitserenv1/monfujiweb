ae1e2372f8a5312129bd3543d5827189b4d8f136
Dalaitserenv1 <128288746+Dalaitserenv1@users.noreply.github.com>
2023-03-19 18:50:12 +0800

    with env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
- 
+  env:{
+    NEXT_PUBLIC_GRAPHCMS_ENDPOINT: 'https://api-ap-northeast-1.hygraph.com/v2/cl8ii99h45vtm01ul0iqldvh5/master'
+  }
 }
 
 module.exports = nextConfig
