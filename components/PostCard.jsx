ee930aea3f11b2fbf41f0cd99f6a717d076bd651
Dalaitserenv1 <128288746+Dalaitserenv1@users.noreply.github.com>
2023-03-19 19:22:51 +0800

    again

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -8,9 +8,10 @@ const PostCard = ({ post }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
       <div className="relative overflow-hidden drop-shadow-2xl pb-80 mb-6">
-        <Image
+        <img
           src={post.featuredImage.url}
           alt={post.title}
+          
           className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
         />
       </div>
@@ -19,10 +20,10 @@ const PostCard = ({ post }) => {
       </h1>
       <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
         <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
-          <Image
+          <img
             alt={post.author.name}
-            height="30px"
-            width="30px"
+            height={30}
+            width={30}
             className="align-middle rounded-full"
             src={post.author.photo.url}
           />
